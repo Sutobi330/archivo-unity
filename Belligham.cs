a54f7da48c3cb6be601f232385de79fa61620b5f
Sutobi330 <152321482+Sutobi330@users.noreply.github.com>
2023-11-28 19:27:11 +0100

    Add files via upload

---
+++ Belligham.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class belligham : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("romper el game object cube");
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
